9764ae4ed85ed39a23624e0bbf0c31a3b8e48599
wimdusphone <101966658+wimdusphone@users.noreply.github.com>
2022-03-20 13:07:52 +0530

    Update and rename aosp_vayu.mk to windows_vayu.mk

--- windows_vayu.mk
+++ windows_vayu.mk
@@ -12,16 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/windows/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := windows_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := Poco X3 Pro
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Poco X3 Pro by Wimdus
+PRODUCT_MANUFACTURER := Windows
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -30,4 +30,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=vayu_global \
     PRODUCT_MODEL=M2102J20SI
 
-BUILD_FINGERPRINT := POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.4.0.RJUMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/vayu_windows/vayu:11/RKQ1.200826.002/V12.5.4.0.RJUMIXM:user/release-keys
