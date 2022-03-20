5307c2726e4b6917cf8e64aa9e798156356ef6c4
wimdusphone <101966658+wimdusphone@users.noreply.github.com>
2022-03-20 13:10:08 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,14 @@ LOC_HIDL_VERSION := 4.0
 # Input
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 304
 
+# Windows
+TARGET_HARD_BRICK := yes
+TARGET_BYPASS_XIAOMI := true
+
+
+
+Target_buildbot := @Sebaubuntu
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -91,7 +99,7 @@ BOARD_KERNEL_IMAGE_NAME := Image
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := vayu_user_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
-  TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
+  TARGET_KERNEL_SOURCE := kernel/windows/vayu
   TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 endif
 
